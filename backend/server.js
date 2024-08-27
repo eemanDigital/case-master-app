5d02b742119c68efcea3a8fea71bdd77414adce2
eemanDigital <eemandigitalconcept@gamil.com>
2024-08-27 15:50:25 +0100

    still fixing cors issue

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://case-master-app.vercel.app"],
+    origin: ["http://localhost:5173", "https://case-master-app.vercel.app/"],
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true,
   })
