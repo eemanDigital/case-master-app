7eabce9cc042e9f4e7401ef33905df8f9634ffcc
eemanDigital <eemandigitalconcept@gamil.com>
2026-03-03 15:24:02 +0100

    resolved build error

--- frontend/src/pages/NoteList.jsx
+++ frontend/src/pages/NoteList.jsx
@@ -32,7 +32,7 @@ import {
   UnorderedListOutlined,
 } from "@ant-design/icons";
 
-import useDebouncedValue from "../hooks/useDebouncedValue";
+import useDebouncedValue from "../hooks/useDebouncedValue.jsx";
 import {
   fetchNotes,
   deleteNote,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: [".mjs", ".js", ".jsx", ".json", ".node"],
   },
   css: {
     preprocessorOptions: {
